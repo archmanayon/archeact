e911f5b93b69c33e70c3c68605662f1b7b6ede29
Archie Manayon <archmanayon@gmail.com>
2024-02-05 22:12:06 +0800

    okay na. if minimum, no button, if more, with buttons: show or hide,, but "..." kulang pa

--- src/component/LongText.tsx
+++ src/component/LongText.tsx
@@ -7,11 +7,15 @@ interface Props{
 const LongText = ({children}:Props) => {
   
   const [lessText, setLessText]=useState(true);
+  const paraButton = children.length > 10 ? 
+    <button onClick={()=> setLessText(!lessText)}> {lessText ?'. . .read more' :'show less'}</button> : "" ;
   
   return (
     <div>          
-        <p>{lessText ? children.substring(0,15) : children}
-        <button onClick={()=> setLessText(!lessText)}> {lessText ?". . .read more" : "show less . . ."}</button>
+        <p>{lessText ? children.substring(0,15): children}
+        
+          {paraButton}
+
         </p>
     </div>
   )
