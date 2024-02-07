537e1e9df0a94c0e10f413bb8b872113a92cb994
Archie Manayon <archmanayon@gmail.com>
2024-02-08 01:47:31 +0800

    can delete items now

--- src/App.tsx
+++ src/App.tsx
@@ -7,14 +7,14 @@ function App() {
 
   const [list, setList] = useState([{ itemName: '', quantity: '', amount: '', group:'' }]);  
 
-  const onAdd = (item) => setList([...list,{itemName:item.name, quantity:item.quantity, amount:item.amount, group:item.group}]);  
+  const onAdd = (item) => setList([...list,{itemName:item.name, quantity:item.quantity, amount:item.amount, group:item.group}]);    
   
   return (
     <div className="App">
        
         <div>
           <Item onAdd = {onAdd}/>
-          <ListOfItems list={list}/>
+          <ListOfItems list={list} handleDelete={(selectIndex)=> setList(list.filter((anItem, index)=> index !==selectIndex))}/>
         </div>
     </div>
   )
--- src/grocery/ListOfItems.tsx
+++ src/grocery/ListOfItems.tsx
@@ -7,33 +7,35 @@ interface Props {
       group: String;
     }
   ];
+  handleDelete?:(selectIndex:Number)=>void;
 }
-const ListOfItems = ({ list }: Props) => {
+const ListOfItems = ({ list, handleDelete }: Props) => {
+  
   return (
     <>
-      <table className="w-full whitespace-nowrap">
+      <table className="w-full whitespace-nowrap border-collapse border border-slate-500">
         <thead>
           <tr className="focus:outline-none h-16 border border-gray-100 rounded">
-            <th>name</th>
-            <th>quantity</th>
-            <th>amount</th>
-            <th>group</th>
+            <th className="border border-slate-600">name</th>
+            <th className="border border-slate-600">quantity</th>
+            <th className="border border-slate-600">amount</th>
+            <th className="border border-slate-600">group</th>
             <th />
           </tr>
         </thead>
         <tbody>
           {list
             .filter((anItem, index) => index !== 0)
-            .map((item, index) => (
+            .map((item, index) => (            
               <tr
                 key={index}
                 className="focus:outline-none h-16 border border-gray-100 rounded"
               >
-                <td>{item.itemName}</td>
-                <td>{item.quantity.toString()}</td>
-                <td>{item.amount.toString()}</td>
-                <td>{item.group}</td>
-                <td>delete</td>
+                <td className="border border-slate-800">{item.itemName}</td>
+                <td className="border border-slate-800">{item.quantity.toString()}</td>
+                <td className="border border-slate-800">{item.amount.toString()}</td>
+                <td className="border border-slate-800">{item.group}</td>
+                <td className="border border-slate-800" ><button onClick={()=>handleDelete(index)} >DELETE</button></td>
               </tr>
             ))}
         </tbody>
